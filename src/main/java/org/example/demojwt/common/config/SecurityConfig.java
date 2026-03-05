71322b360cb2a8da5ccce95e03a429356aada826
duyddcd <duydd@syn-gr.com>
2026-03-05 16:14:17 +0700

    develop : add api

--- src/main/java/org/example/demojwt/common/config/SecurityConfig.java
+++ src/main/java/org/example/demojwt/common/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/h2-console/**",
                                                                       "/api*",
+                                                                      "/api/health",
                                                                       "/socket.io/**",
                                                                       "/api/auth/**",
                                                                       "/error",
